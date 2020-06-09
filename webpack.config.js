7b3b86a1df47ee51c2c86a20405fd1c40dfbc687
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-06-09 13:05:58 +0200

    adding css

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: ['./students.js'],
+  entry: {
+    app: './students.js',
+    appStyles: ['./mystyles.css'],
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
   },
   module: {
     rules: [
@@ -11,13 +14,24 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html', //Name of file in ./dist/
       template: 'index.html', //Name of template in ./src
-      hash: true,
     }),
   ],
   devServer: {
